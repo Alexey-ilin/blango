42498049c45aeb75a9414ed0e7714b5f5c47b6dc
Ильин Сергеевич <alexey_ilin12@mail.ru>
2022-05-23 13:41:27 +0000

    Finish serailizers

--- blango/settings.py
+++ blango/settings.py
@@ -85,6 +85,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework',
     ]
 
     MIDDLEWARE = [
